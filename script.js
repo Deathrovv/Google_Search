e5e1aba9a761604456d98fee46495ebfb96b65e1
Deathrovv <aro.maru7ian@gmail.com>
2021-05-06 21:15:14 +0200

    done for the day. Adding dark mode tomorrow

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //Add font to icons via JS
 //Implement responsiveness
+
+const darkSwitch = document.querySelector(".switch");
+const darkItems = document.querySelectorAll(".dm");
+
+// darkSwitch.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   darkItems.forEach((item) => item.classList.toggle("dark"));
+// });

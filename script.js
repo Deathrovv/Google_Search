df905e6edc02277d2a550221f8df4969a47f7de5
Deathrovv <aro.maru7ian@gmail.com>
2021-05-07 16:55:50 +0200

    all done, only thing left is to implement manual dark mode switch

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //Add font to icons via JS
 //Implement responsiveness
 
-const darkSwitch = document.querySelector(".switch");
-const darkItems = document.querySelectorAll(".dm");
+// const darkSwitch = document.querySelector(".switch");
+// const darkItems = document.querySelectorAll(".dm");
 
 // darkSwitch.addEventListener("click", (e) => {
 //   e.preventDefault();
 //   darkItems.forEach((item) => item.classList.toggle("dark"));
 // });
+
+// var theme,
+//   prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+// if (prefersDarkScheme.matches)
+//   theme = document.body.classList.contains("light-mode") ? "light" : "dark";
+// else theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+// localStorage.setItem("theme", theme);
+
+// function toggle() {
+//   var currentTheme = localStorage.getItem("theme");
+//   if (currentTheme == "dark") document.body.classList.toggle("light-mode");
+//   else if (currentTheme == "light") document.body.classList.toggle("dark-mode");
+// }

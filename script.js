ae2a72d23032bf7ae095a3aaf56c07ce33ec1c3e
Deathrovv <aro.maru7ian@gmail.com>
2021-05-17 00:47:01 +0300

    Project done

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-//Add font to icons via JS
-//Implement responsiveness
+const darkSwitch = document.querySelector(".switch");
 
-// const darkSwitch = document.querySelector(".switch");
-// const darkItems = document.querySelectorAll(".dm");
+const themeSwitchF = function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const switchToTheme = currentTheme === "dark" ? "light" : "dark";
+  document.documentElement.setAttribute("data-theme", switchToTheme);
+};
 
-// darkSwitch.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   darkItems.forEach((item) => item.classList.toggle("dark"));
+darkSwitch.addEventListener("click", themeSwitchF);
+
+// document.addEventListener("DOMContentLoaded", function (e) {
+//   document.documentElement.setAttribute("data-theme", "light");
+
 // });
-
-// var theme,
-//   prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-// if (prefersDarkScheme.matches)
-//   theme = document.body.classList.contains("light-mode") ? "light" : "dark";
-// else theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
-// localStorage.setItem("theme", theme);
-
-// function toggle() {
-//   var currentTheme = localStorage.getItem("theme");
-//   if (currentTheme == "dark") document.body.classList.toggle("light-mode");
-//   else if (currentTheme == "light") document.body.classList.toggle("dark-mode");
-// }
